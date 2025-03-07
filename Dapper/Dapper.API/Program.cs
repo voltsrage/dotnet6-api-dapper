bc1970502aa30f36a3f73211303575157fe2f31a
voltsrage <electric5388@hotmail.com>
2025-03-07 20:13:16 +0700

    Add logging with Serilog which allows for logs to 1. Seq 2. ElasticSearch 3. Console 4. SQL Server

--- Dapper/Dapper.API/Program.cs
+++ Dapper/Dapper.API/Program.cs
@@ -1,42 +1,62 @@
 using Dapper.API.Data.Dapper;
+using Dapper.API.Logging;
 using Dapper.API.Models.AppSettings;
-using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-#region AppSettings Configuration
-var connectionStrings = new ConnectionStrings();
-builder.Configuration.Bind(key: nameof(ConnectionStrings), connectionStrings);
-builder.Services.AddSingleton(connectionStrings);
-
-#endregion AppSettings Configuration
-
-#region Data Access Dependency Injection
-builder.Services.AddSingleton<IDapperHandler, DapperHandler>();
-#endregion
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+    #region Serilog
+    builder.Host.UseSerilog(SeriLogger.Configure);
+    #endregion Serilog
+
+    #region AppSettings Configuration
+    var connectionStrings = new ConnectionStrings();
+    builder.Configuration.Bind(key: nameof(ConnectionStrings), connectionStrings);
+    builder.Services.AddSingleton(connectionStrings);
+    #endregion AppSettings Configuration
+
+    #region Data Access Dependency Injection
+    builder.Services.AddSingleton<IDapperHandler, DapperHandler>();
+    #endregion
+
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
